c34d562ec97c824d362f072b6c05d56d62ddac84
My Nguyen <mynt1909@gmail.com>
2015-05-30 10:00:28 -0700

    view for player, check auth view

--- application/modules/User/Form/Playercard/Edit.php
+++ application/modules/User/Form/Playercard/Edit.php
@@ -4,5 +4,7 @@ class User_Form_Playercard_Edit extends User_Form_Playercard_Create
   public function init()
   {
     parent::init();
+	$this -> setTitle('Edit Player Card');
+	$this->submit->setLabel('Save Changes');
   }
 }
