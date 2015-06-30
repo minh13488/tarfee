989f6860525a8228b95aac22e7008fcf7fc27b77
My Nguyen <mynt1909@gmail.com>
2015-06-30 12:49:44 -0700

    check used invite code

--- application/modules/Payment/Api/Core.php
+++ application/modules/Payment/Api/Core.php
@@ -18,5 +18,49 @@
  */
 class Payment_Api_Core extends Core_Api_Abstract
 {
-  
+  	public function checkValidCode($referCode) {
+  		
+  	  $isEnabled = Engine_Api::_()->getApi('settings', 'core')->getSetting('user_referral_enable', 1);
+	  $period = Engine_Api::_()->getApi('settings', 'core')->getSetting('user_referral_trial', 0);
+	  $now =  date("Y-m-d H:i:s");
+	  
+  	  $invite = Engine_Api::_() -> invite() -> getRowCode($referCode);
+	  
+	  if($isEnabled && $invite)
+	  {
+	  	  //if exist code then get expire date
+		  if($period == 1)
+		  {
+				$type = 'day';
+	      }
+		  else 
+		  {
+				$type = 'days';
+		  }
+		  $expiration_date = date_add(date_create($invite->timestamp),date_interval_create_from_date_string($period." ".$type));
+		  
+		  $nowDate = date_create($now);
+		  
+	  	  if($period != 0) 
+	  	  {
+	  	  	  if ($nowDate >= $expirationDate) 
+			  {
+			  	return true;
+			  }
+			  else
+			  {
+			  	return false;
+			  }
+	  	  }
+		  else 
+		  {
+		  	   //if code never expired
+	  	   		return true;
+		  }
+	  }
+	  else 
+	  {
+	  	return false;
+	  }
+  	}
 }
