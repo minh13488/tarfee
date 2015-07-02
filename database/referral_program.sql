a5ff257d04791e2f9d9804d2dd39ac0d21c90f7d
My Nguyen <mynt1909@gmail.com>
2015-07-02 12:03:14 -0700

    Total number and list of generated referral codes.

--- database/referral_program.sql
+++ database/referral_program.sql
@@ -2,6 +2,7 @@ INSERT IGNORE INTO `engine4_core_menuitems` (`name`, `module`, `label`, `plugin`
 ('core_admin_main_settings_referral', 'user', 'Referral Program Settings', '', '{"route":"admin_default","module":"user","controller":"referral-settings","action":"global"}', 'core_admin_main_settings', '', 999);
 
 INSERT IGNORE INTO `engine4_core_menuitems` (`name`, `module`, `label`, `plugin`, `params`, `menu`, `submenu`, `order`) VALUES
+('referral_admin_main_referralcodes', 'user', 'Referral Codes', '', '{"route":"admin_default","module":"user","controller":"referral-codes","action":"generated"}', 'referral_admin_main', '', 3),
 ('referral_admin_main_levelsettings', 'user', 'Member Level Settings', '', '{"route":"admin_default","module":"user","controller":"referral-settings","action":"level"}', 'referral_admin_main', '', 2),
 ('referral_admin_main_globalsettings', 'user', 'Global Settings', '', '{"route":"admin_default","module":"user","controller":"referral-settings","action":"global"}', 'referral_admin_main', '', 1);
 
